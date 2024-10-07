dd21af80424b2f631bf7672a886366675a04a94e
krisnadibyo <krisna_itbstei@hotmail.com>
2024-10-07 16:03:06 +0700

    solution

---
+++ src/components/MainWrapper/MainWrapper.js
@@ -0,0 +1,22 @@
+'use client';
+
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function MainWrapper({ children }) {
+  const Tag = styled.main`
+  width: 100%;
+  max-width: 800px;
+  padding: 16px 24px;
+  margin: 0px auto;
+  border: 1px solid hsl(0deg 0% 50% / 0.3);
+  border-radius: 2px;
+  background: white;
+`;
+  return (
+    <Tag>
+      {children}
+    </Tag>
+  )
+}
